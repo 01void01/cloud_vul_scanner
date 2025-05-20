d0e143761cd53231fea6b4c8722383d9951c9b9f
vOid <107850224+01void01@users.noreply.github.com>
2025-05-20 12:46:25 +0530

    Update app.py
    flask port update for render deploy

--- cloud_vuln_scanner_web/app.py
+++ cloud_vuln_scanner_web/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from app.routes import routes_bp
+import os
 
 app = Flask(__name__, template_folder='app/templates')
 app.secret_key = "super-secret-key"
@@ -7,4 +8,6 @@ app.secret_key = "super-secret-key"
 app.register_blueprint(routes_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
